786431bad3fe5a4222418972a2865f0c5482c11f
aakashDeveloper <ahanda205@gmail.com>
2019-11-21 16:07:40 +0000

    a

---
+++ first.js
@@ -0,0 +1,137 @@
+String ="aaa" 'aa'  "a' '10' 'true'
+Number = 877, 98.99
+Boolean= true/false
+Es5( Ecma Script 5)
+
+
+var a = 10
+undefined
+a
+10
+var b = "abc"
+undefined
+var c = true
+undefined
+typeof(a)
+"number"
+typeof(b)
+"string"
+typeof(c)
+"boolean"
+var d = "10"
+undefined
+typeof(d)
+"string"
+var e = "true"
+undefined
+typeof(e)
+"string"
+////////////////////////////
+
+var a = 10
+var b = 20
++ - * / %
+
+a+b
+30
+a-b
+-10
+a/b
+0.5
+a*b
+200
+a%b
+10
+
+var a = "hi"
+var b =" javascript"
+
+var a = "hi"
+var b =" javascript"
+undefined
+a+b
+"hi javascript"
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+a%b
+NaN
+
+var a = 10
+var b = "hi"
+var c = 10
+
+var a = 10
+var b = "hi"
+var c = 10
+undefined
+a+b
+"10hi"
+var out = a+b
+undefined
+out
+"10hi"
+typeof(out)
+"string"
+b+c
+"hi10"
+a+c
+20
+
+
+number+number = number
+string+string = string
+string+ number = string
+number+string = string
+
+number+string = string
+add+string = conact
+
+15+10+"hi"  = "25hi"
+"hi"+10+15  = "hi25"
+
+4%2
+0
+5%2
+1
+5%3
+2
+6%1
+0
+6%3
+0
+2%4
+2
+3%4
+3
+1%2
+1
+
+1+1
+true +true
+2
+1+0
+true+false
+1
+0+0
+false+false
+0
+
+0+1
+false+true
+1
+
+true equal to  1
+false equal to 0
+
+var a = "hi"
+undefined
+var b = true
+undefined
+
+String+Boolean = string
+Boolean+Boolean = number
+Number+Boolean = number
