c0e7b8efe60fe7ac23b98b43ab2d3c851c2dc9dc
aakashDeveloper <ahanda205@gmail.com>
2019-11-22 16:15:42 +0000

    a

---
+++ second.js
@@ -0,0 +1,53 @@
+=   assignment operator
+== comparision operator just data
+=== comparision operator just data & data type
+
+
+var a = 10
+undefined
+a
+10
+var a = 20
+undefined
+a
+20
+a == 10
+false
+a == 20
+true
+var c = '20'
+undefined
+c == 20
+true
+c === 20
+false
+a === 20
+true
+
+
+////es5////
+var a = 10
+a
+10
+var a = 20
+a
+20
+
+
+///es6////
+let
+const
+
+var = we decalre and reassign
+let = we cannot decalre but can reassign
+const = we cannot and  cannot reassign
+
+
+
+
+
+
+var subject ='javascript';
+var subject => declare a varible
+subject = 'javascript';  => assignment 
+
---
+++ third.js
@@ -0,0 +1,67 @@
+console.log
+alert
+confirm
+prompt
+
+
+console.log("hiii")
+Use in debgging the application
+
+
+alert("You are correct")
+alert("hii")
+Act like a prompt and use for alert message
+
+confirm("Do you want leave")
+true
+confirm("Do you want leave")
+false
+var output = confirm("Do you want leave")
+undefined
+output
+true
+var output = confirm("Do you want leave")
+undefined
+output
+false
+
+>> Return boolean value on basis of user choice. 
+
+prompt("what is your name")
+"aakash"
+var name = prompt("what is your name")
+undefined
+name
+"John"
+>> Use take input from the user
+
+
+var a = prompt('enter first value')
+var b = prompt('enter second value')
+var output = parseFloat(a)+parseFloat(b)
+console.log(output)
+
+
+var a = "10"
+undefined
+parseInt(a)
+10
+var a = "hi"
+undefined
+parseInt(a)
+NaN
+var a = "10.999"
+undefined
+parseInt(a)
+10
+parseFloat(a)
+10.999
+
+Task 
+> Take input from the user and greet the user
+> You need figure out 
+    > var a = 10
+    > var b = 10
+    > a+b = 20 
+    but you need to get output as "1010"
+
